4efb665829ca21cea940607593621cefec047617
wongpakho814 <wongpakho814@gmail.com>
2022-10-25 15:28:55 +1100

    Added delete route and functionality to notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const notes = require("express").Router();
 const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
 const uniqid = require("uniqid");
+const fs = require("fs");
 
 // GET Route that reads the db.json file and return all saved notes as JSON
 notes.get("/", (req, res) => {
@@ -27,6 +28,35 @@ notes.post("/", (req, res) => {
   } else {
     res.error("Error in adding note");
   }
+});
+
+// DELETE Route that deletes a note
+notes.delete("/:id", (req, res) => {
+  const noteId = req.params.id;
+  console.info(`${req.method} request received to delete note ${noteId}`);
+
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      let parsedNotes = JSON.parse(data);
+
+      // Filter and return an array of JSON object that does not contain the item with the specified note ID
+      parsedNotes = parsedNotes.filter((item) => item.id !== noteId);
+
+      // Write updated notes back to the file
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(parsedNotes, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info("No error detected while writing!")
+      );
+    }
+  });
+
+  res.json(`Successfully deleted note ${noteId}!`);
 });
 
 module.exports = notes;
